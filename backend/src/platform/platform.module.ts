5f01132ab79bc6cbd39ccc4c5a8f9de7350fbef4
Moein <moein.molla.7392@gmail.com>
2026-08-07 15:28:28 +0330

    RM-05: separate platform entities from Nest module

--- backend/src/platform/platform.module.ts
+++ backend/src/platform/platform.module.ts
@@ -1,51 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from '../products/entities/product.entity';
-import { AuditLogEntity } from './audit/entities/audit-log.entity';
 import { AuditService } from './audit/audit.service';
-import {
-  ImportRunEntity,
-  MappingTemplateEntity,
-} from './import/entities/import-run.entity';
-import { ProductVariationEntity } from './import/entities/product-variation.entity';
 import { ImportService } from './import/import.service';
-import { PlatformJobEntity } from './jobs/entities/platform-job.entity';
 import { JobsService } from './jobs/jobs.service';
-import {
-  InventoryAuditEntity,
-  MediaAssetEntity,
-} from './media/entities/media-asset.entity';
 import { MediaService } from './media/media.service';
 import { MerchandisingService } from './merchandising/merchandising.service';
 import {
   PlatformController,
   PlatformPublicController,
 } from './platform.controller';
-import {
-  AttributeValueEntity,
-  CuratedLookEntity,
-  MerchRuleEntity,
-  ProductTagEntity,
-  RecommendationEventEntity,
-  TaxonomyTagEntity,
-} from './taxonomy/entities/taxonomy.entities';
+import { PLATFORM_ENTITIES } from './platform.entities';
 import { WorkflowService } from './workflow/workflow.service';
 
-export const PLATFORM_ENTITIES = [
-  PlatformJobEntity,
-  AuditLogEntity,
-  ImportRunEntity,
-  MappingTemplateEntity,
-  ProductVariationEntity,
-  MediaAssetEntity,
-  InventoryAuditEntity,
-  TaxonomyTagEntity,
-  ProductTagEntity,
-  AttributeValueEntity,
-  MerchRuleEntity,
-  CuratedLookEntity,
-  RecommendationEventEntity,
-];
+export { PLATFORM_ENTITIES } from './platform.entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([...PLATFORM_ENTITIES, ProductEntity])],
